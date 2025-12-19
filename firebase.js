0aacb9bb9821476318d2f991b03a03dca938d5a4
DonBabu-ux <lewismwangi990@gmail.com>
2025-12-20 01:13:57 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,19 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAkMFYizmJ4dd8G8oJ9uw1JdgzwhtmsENU",
   authDomain: "mywebapp-bb780.firebaseapp.com",
   projectId: "mywebapp-bb780",
-  storageBucket: "mywebapp-bb780.appspot.com",
+  storageBucket: "mywebapp-bb780.firebasestorage.app",
   messagingSenderId: "1020137155241",
-  appId: "1:1020137155241:web:5f120fc16d62249fd897b8"
+  appId: "1:1020137155241:web:7f3b26f9f680ee01d897b8"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const rtdb = getDatabase(app);
+
