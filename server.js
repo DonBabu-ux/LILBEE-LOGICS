f4870c1eb2f48a1b886eb7b346452835dfbdb9c3
superomosh94 <omondo94@gmail.com>
2025-12-27 12:55:06 +0300

    Fix root route to serve index.html

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(__dirname));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 async function readDB() {
     return await fs.readJson(DB_FILE);
 }
